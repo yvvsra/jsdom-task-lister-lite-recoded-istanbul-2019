662866bbf53fa07cc5752add08ac4601ed23b9ad
yvvsra <yvvsra1@gmail.com>
2020-01-29 13:43:22 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded",() => {
+   // Use id of the form  to prevent the submit button from working 
+    document.getElementById("create-task-form").addEventListener("submit",function(event){
+    event.preventDefault();
+}, false);
+ }); 
