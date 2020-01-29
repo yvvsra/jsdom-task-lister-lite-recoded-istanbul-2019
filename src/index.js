5add969573f1c38ad5001467acf46ce9047ed813
Yusra Atta <yvvsra1@gmail.com>
2020-01-29 21:11:46 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded",() => {
-   document.getElementById("new-task-description").addEventListener("click",function(event)
+   document.getElementById("new-task-description").addEventListener("submit",function(event)
     event.preventDefault() )
    
 });
